4b49402e6c50f18af1f5f6517ee3d86177a5481e
이석원 <lsw2524@naver.com>
2021-02-04 17:23:55 +0900

    S04P13A305-17 [u] editPostImg

--- frontend/src/components/sub2/BoardUpdate.vue
+++ frontend/src/components/sub2/BoardUpdate.vue
@@ -11,20 +11,36 @@
       v-model="board.postInfo.content"
       :value="board.postInfo.content"
     ></v-textarea>
-
+    <input
+      multiple="multiple"
+      ref="file"
+      type="file"
+      id="file"
+      name="file"
+      @change="onChangeImages"
+    />
     <div>
       <v-row align="center">
-        <span v-for="(item, index) in imgUrl" :key="index">
-          <v-col>
-            <img :src="item" :id="'img' + index" width="300px" height="300px" />
-            <input
+        <span>
+          <v-col v-for="(item, idx) in imgUrl" :key="idx">
+            <img :src="item.modPicName" :id="'img' + idx" width="300px" height="300px" />
+            <!-- <input
               ref="file"
               type="file"
               id="file"
               name="file"
               @change="onChangeImages($event, item, index)"
-            />
+            /> -->
+            <v-btn color="red darken-1" @click="modiImg(item,idx)">
+            Delete
+          </v-btn>
           </v-col>
+          <v-col cols="5" v-for="(imgUrl, index) in imageUrl" :key="index">
+          <v-img max-width="800px" max-height="800px" :src="imgUrl"></v-img>
+          <v-btn color="red darken-1" @click="deleteImg(index)">
+            Delete
+          </v-btn>
+        </v-col>
         </span>
       </v-row>
     </div>
@@ -43,6 +59,9 @@ export default {
       not: [],
       modified: [],
       temp: [],
+      myfile: [],
+      imageUrl: [],
+      deleted: [],
     };
   },
   created() {
@@ -63,13 +82,9 @@ export default {
         }
         console.log('템프는',this.temp)
         for (var i = 0; i < response.data.fileList.length; i++) {
-          this.imgUrl.push(
-            `${SERVER_URL}/post/imgs/download?fileName=` +
-              response.data.fileList[i].modPicName +
-              "&postNo=" +
-              this.$route.query.board.postInfo.postNo
-          );
+          this.imgUrl.push(response.data.fileList[i]);
         }
+        console.log(this.imgUrl)
         this.board = response.data;
       })
       .catch((error) => {
@@ -78,30 +93,45 @@ export default {
   },
 
   methods: {
-   modifyComplete() {
+    modiImg(item, index) {
+      this.deleted.push(item.picNo)
+      this.imgUrl.splice(index, 1)
+      console.log(item)
+      console.log(index)
+    },
+    deleteImg(index) {
+      console.log(index)
+      this.imageUrl.splice(index, 1)
+      this.myfile.splice(index, 1)
+    },
+    modifyComplete() {
       var frm = new FormData();
-      console.log('모디파ㄴ이는',this.modified)
-    for (var i = 0; i < this.$refs.file.length; i++) {
-        if(this.$refs.file[i].files.length>0){
-            frm.append("files", this.$refs.file[i].files[0]);
-        }
+      console.log(this.myfile)
+      for (var i = 0; i < this.myfile.length; i++) {
+        let file = this.myfile[i];
+        frm.append('files', file);
       }
+    // for (var i = 0; i < this.$refs.file.length; i++) {
+    //     if(this.$refs.file[i].files.length>0){
+    //         frm.append("files", this.$refs.file[i].files[0]);
+    //     }
+    //   }
       console.log(frm);
       for (var i = 0; i < this.modified.length; i++) {
         const idx = this.temp.indexOf(this.modified[i]);
         this.temp.splice(idx,1);
       }
-      for (var i = 0; i < this.temp.length; i++){
-        frm.append("unmodified", this.temp[i]);
+      // for (var i = 0; i < this.temp.length; i++){
+      //   frm.append("unmodified", this.temp[i]);
 
-      }
+      // }
       if (this.modified.length <1) {
         console.log('변경된거 없음')
         this.temp = []
       }
       console.log('모디파이드 배열은',this.modified)
       console.log('최종보내줄 배열은',this.temp)
-
+      frm.append("unmodified",this.deleted)
       frm.append("postNo", this.board.postInfo.postNo);
       frm.append("content", this.board.postInfo.content);
       frm.append("title", this.board.postInfo.title);
@@ -115,23 +145,33 @@ export default {
           alert("수정 실패");
         });
     },
-    onChangeImages(event, item, index) {
-      document.getElementById("img" + index).src = URL.createObjectURL(
-        event.target.files[0]
-      );
-      console.log(event);
-      this.not[index] = event.target.files[0];
-      this.modified = [];
-      for (var n = 0; n < this.not.length; n++) {
-        //   console.log(this.not[n].picNo)
-        if (this.not[n].picNo) {
-          this.modified.push(this.not[n].picNo);
-        }
-        //   console.log(this.not[n])
+    onChangeImages(e) {
+      var file = e.target.files;
+      for (var i = 0; i < file.length; i++) {
+        this.imageUrl.push(URL.createObjectURL(file[i]));
+        this.myfile.push(this.$refs.file.files[i])
       }
-      //   this.imgUrl[0] = URL.createObjectURL(event.target.files[0]);
-      console.log(this.modified);
+      console.log(this.myfile)
+      console.log(this.imageUrl)
     },
+    // onChangeImages(e) {
+    //   this.myfile.push(event.target.files[0])
+    //   document.getElementById("img" + index).src = URL.createObjectURL(
+    //     event.target.files[0]
+    //   );
+    //   console.log(event);
+    //   this.not[index] = event.target.files[0];
+    //   this.modified = [];
+    //   for (var n = 0; n < this.not.length; n++) {
+    //     //   console.log(this.not[n].picNo)
+    //     if (this.not[n].picNo) {
+    //       this.modified.push(this.not[n].picNo);
+    //     }
+    //     //   console.log(this.not[n])
+    //   }
+    //   //   this.imgUrl[0] = URL.createObjectURL(event.target.files[0]);
+    //   console.log(this.modified);
+    // },
   },
   computed: {},
 };
