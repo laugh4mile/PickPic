2a6bc51ff2fa3eb79a376df9dcd77c26e0fe5c29
JH_Joo <wjdgnsx94@naver.com>
2021-02-09 14:40:14 +0900

    S04P13A305-49 [+] visualize Vote Result

--- frontend/src/components/sub2/BoardDetail.vue
+++ frontend/src/components/sub2/BoardDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" v-if="!loading">
     <div>
       <v-row>
         <v-col>
@@ -33,29 +33,35 @@
     </div>
     <hr />
     <div class="contents" v-html="cont"></div>
-    <vote :no="board.postInfo.postNo" />
+    <Vote :no="board.postInfo.postNo" />
     <div>
       <v-btn
         :disabled="this.board.postInfo.email != $store.getters.getUserEmail"
-        @click="modifyForm"  :contents="this.cont"
+        @click="modifyForm"
+        :contents="this.cont"
         >수정</v-btn
       >
       <v-dialog v-model="dialog1" width="230">
         <template v-slot:activator="{ on, attrs }" @click="dialog1 = true">
-          <v-btn :disabled="board.postInfo.email != $store.getters.getUserEmail" variant="primary" v-bind="attrs" v-on="on">
+          <v-btn
+            :disabled="board.postInfo.email != $store.getters.getUserEmail"
+            variant="primary"
+            v-bind="attrs"
+            v-on="on"
+          >
             게시글삭제
           </v-btn>
         </template>
         <v-card>
           <v-card-title>
-              삭제하시겠습니까
-            </v-card-title>
+            삭제하시겠습니까
+          </v-card-title>
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="red" @click="deleteForm">
               삭제
             </v-btn>
-            <v-btn color="primary" @click="dialog1=false">
+            <v-btn color="primary" @click="dialog1 = false">
               취소
             </v-btn>
           </v-card-actions>
@@ -84,31 +90,33 @@
   </div>
 </template>
 <script>
-import Vote from './vote.vue';
-import Comment from './Comment.vue';
-import axios from 'axios';
-import { mapGetters } from 'vuex';
+import Vote from "./vote.vue";
+import Comment from "./Comment.vue";
+import axios from "axios";
+import { mapGetters } from "vuex";
 const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 export default {
   data() {
     return {
       board: {},
-      no: '',
-      heartIcon: '',
+      no: "",
+      heartIcon: "",
       like: false,
       dialog1: false,
       items: ["취소", "삭제"],
-      cont:'',
+      cont: "",
+      loading: false,
     };
   },
   computed: {
-    ...mapGetters(['getAccessToken', 'getUserEmail', 'getUserName', 'getRole']),
+    ...mapGetters(["getAccessToken", "getUserEmail", "getUserName", "getRole"]),
   },
   components: {
     Vote,
     Comment,
   },
   created() {
+    this.loading = true;
     axios
       .get(`${SERVER_URL}/post`, {
         params: {
@@ -120,13 +128,14 @@ export default {
         console.log(response.data);
         this.board = response.data;
         this.cont = this.board.postInfo.content;
+        this.loading = false;
         console.log(this.cont);
         document.getElementById("editor").innerHTML = this.cont;
-        if (this.board.likeCheck == 'N') {
-          this.heartIcon = require('@/assets/blank heart.png');
+        if (this.board.likeCheck == "N") {
+          this.heartIcon = require("@/assets/blank heart.png");
           this.like = false;
         } else {
-          this.heartIcon = require('@/assets/heart.jpg');
+          this.heartIcon = require("@/assets/heart.jpg");
           this.like = true;
         }
       })
@@ -137,15 +146,15 @@ export default {
   methods: {
     menuclick(event, cmt) {
       if (event.target.innerText == "삭제") {
-        this.deleteForm()
+        this.deleteForm();
       }
     },
     toBoard() {
-      this.$router.push('/board');
+      this.$router.push("/board");
     },
     modifyForm() {
       this.$router.push({
-        path: '/board/update',
+        path: "/board/update",
         query: {
           board: this.board,
         },
@@ -153,11 +162,11 @@ export default {
     },
     heartClick() {
       if (!this.getUserEmail) {
-        alert('로그인이 필요한 서비스입니다.');
+        alert("로그인이 필요한 서비스입니다.");
       } else {
         const params = new URLSearchParams();
-        params.append('email', this.$store.getters.getUserEmail);
-        params.append('postNo', this.board.postInfo.postNo);
+        params.append("email", this.$store.getters.getUserEmail);
+        params.append("postNo", this.board.postInfo.postNo);
 
         axios
           .put(`${SERVER_URL}/post/like`, params)
@@ -172,11 +181,11 @@ export default {
               .then((response) => {
                 console.log(response.data);
                 this.board = response.data;
-                if (this.board.likeCheck == 'N') {
-                  this.heartIcon = require('@/assets/blank heart.png');
+                if (this.board.likeCheck == "N") {
+                  this.heartIcon = require("@/assets/blank heart.png");
                   this.like = false;
                 } else {
-                  this.heartIcon = require('@/assets/heart.jpg');
+                  this.heartIcon = require("@/assets/heart.jpg");
                   this.like = true;
                 }
               })
@@ -197,7 +206,7 @@ export default {
           },
         })
         .then((response) => {
-          this.$router.push('/board');
+          this.$router.push("/board");
         })
         .catch((error) => {
           console.log(error);
